eac20c5ffdc772f5d089df8df65e3b2fcab8f71b
bradley.cantrell <cantrellbldev@gmail.com>
2021-03-04 17:20:18 -0500

    created functions for main operations

---
+++ app.js
@@ -0,0 +1,29 @@
+//ADD
+function addNumbers(x, y){
+    let sum = x + y;
+    return sum;
+}
+
+//SUBTRACT
+function subtract(x,y){
+    let difference = x - y;
+    return difference;
+}
+
+//MULTIPLY
+function multiply(x,y){
+    let product = x * y;
+    return product;
+}
+
+//DIVIDE
+function divide(x,y){
+    let quotient = x/y;
+    return quotient;
+}
+
+//RUN OPERATION
+function operate(operator,x,y){
+    let result = operator(x,y);
+    return result;
+}
